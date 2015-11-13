c69eb97216364cff7640ba80a75d84218962a5d5
spencerkerber <spencer.kerber@me.com>
2015-11-13 16:15:41 -0500

    Complete

---
+++ Program 2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Last_name = new System.Windows.Forms.Label();
+            this.grade_selelection_groupbox = new System.Windows.Forms.GroupBox();
+            this.Senior = new System.Windows.Forms.RadioButton();
+            this.Junior = new System.Windows.Forms.RadioButton();
+            this.Sophomore = new System.Windows.Forms.RadioButton();
+            this.Freshman = new System.Windows.Forms.RadioButton();
+            this.last_name_textbox = new System.Windows.Forms.TextBox();
+            this.registration_button = new System.Windows.Forms.Button();
+            this.grade_selelection_groupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Last_name
+            // 
+            this.Last_name.AutoSize = true;
+            this.Last_name.Location = new System.Drawing.Point(56, 153);
+            this.Last_name.Name = "Last_name";
+            this.Last_name.Size = new System.Drawing.Size(178, 13);
+            this.Last_name.TabIndex = 1;
+            this.Last_name.Text = "Enter the first letter of your last name";
+            // 
+            // grade_selelection_groupbox
+            // 
+            this.grade_selelection_groupbox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.grade_selelection_groupbox.Controls.Add(this.Senior);
+            this.grade_selelection_groupbox.Controls.Add(this.Junior);
+            this.grade_selelection_groupbox.Controls.Add(this.Sophomore);
+            this.grade_selelection_groupbox.Controls.Add(this.Freshman);
+            this.grade_selelection_groupbox.Location = new System.Drawing.Point(45, 12);
+            this.grade_selelection_groupbox.Name = "grade_selelection_groupbox";
+            this.grade_selelection_groupbox.Size = new System.Drawing.Size(200, 126);
+            this.grade_selelection_groupbox.TabIndex = 2;
+            this.grade_selelection_groupbox.TabStop = false;
+            this.grade_selelection_groupbox.Text = "Select your grade";
+            this.grade_selelection_groupbox.Enter += new System.EventHandler(this.grade_selelection_groupbox_Enter);
+            // 
+            // Senior
+            // 
+            this.Senior.AutoSize = true;
+            this.Senior.Location = new System.Drawing.Point(60, 89);
+            this.Senior.Name = "Senior";
+            this.Senior.Size = new System.Drawing.Size(55, 17);
+            this.Senior.TabIndex = 3;
+            this.Senior.TabStop = true;
+            this.Senior.Text = "Senior";
+            this.Senior.UseVisualStyleBackColor = true;
+            // 
+            // Junior
+            // 
+            this.Junior.AutoSize = true;
+            this.Junior.Location = new System.Drawing.Point(60, 66);
+            this.Junior.Name = "Junior";
+            this.Junior.Size = new System.Drawing.Size(53, 17);
+            this.Junior.TabIndex = 2;
+            this.Junior.TabStop = true;
+            this.Junior.Text = "Junior";
+            this.Junior.UseVisualStyleBackColor = true;
+            // 
+            // Sophomore
+            // 
+            this.Sophomore.AutoSize = true;
+            this.Sophomore.Location = new System.Drawing.Point(60, 43);
+            this.Sophomore.Name = "Sophomore";
+            this.Sophomore.Size = new System.Drawing.Size(79, 17);
+            this.Sophomore.TabIndex = 1;
+            this.Sophomore.TabStop = true;
+            this.Sophomore.Text = "Sophomore";
+            this.Sophomore.UseVisualStyleBackColor = true;
+            // 
+            // Freshman
+            // 
+            this.Freshman.AutoSize = true;
+            this.Freshman.Location = new System.Drawing.Point(60, 20);
+            this.Freshman.Name = "Freshman";
+            this.Freshman.Size = new System.Drawing.Size(71, 17);
+            this.Freshman.TabIndex = 0;
+            this.Freshman.TabStop = true;
+            this.Freshman.Text = "Freshman";
+            this.Freshman.UseVisualStyleBackColor = true;
+            // 
+            // last_name_textbox
+            // 
+            this.last_name_textbox.Location = new System.Drawing.Point(93, 181);
+            this.last_name_textbox.Name = "last_name_textbox";
+            this.last_name_textbox.Size = new System.Drawing.Size(100, 20);
+            this.last_name_textbox.TabIndex = 3;
+            // 
+            // registration_button
+            // 
+            this.registration_button.Location = new System.Drawing.Point(84, 227);
+            this.registration_button.Name = "registration_button";
+            this.registration_button.Size = new System.Drawing.Size(119, 23);
+            this.registration_button.TabIndex = 4;
+            this.registration_button.Text = "When can I register?";
+            this.registration_button.UseVisualStyleBackColor = true;
+            this.registration_button.Click += new System.EventHandler(this.registration_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.registration_button);
+            this.Controls.Add(this.last_name_textbox);
+            this.Controls.Add(this.grade_selelection_groupbox);
+            this.Controls.Add(this.Last_name);
+            this.Name = "Form1";
+            this.Text = "Program 2";
+            this.grade_selelection_groupbox.ResumeLayout(false);
+            this.grade_selelection_groupbox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Last_name;
+        private System.Windows.Forms.GroupBox grade_selelection_groupbox;
+        private System.Windows.Forms.RadioButton Sophomore;
+        private System.Windows.Forms.RadioButton Freshman;
+        private System.Windows.Forms.RadioButton Senior;
+        private System.Windows.Forms.RadioButton Junior;
+        private System.Windows.Forms.TextBox last_name_textbox;
+        private System.Windows.Forms.Button registration_button;
+    }
+}
+
---
+++ Program 2/Form1.cs
@@ -0,0 +1,189 @@
+﻿//Spencer Kerber
+// CIS 199-01
+// Project 2 - registration date and time 
+// due 10/27/14
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void grade_selelection_groupbox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registration_button_Click(object sender, EventArgs e)
+        {
+            const string SENIOR_DATE = "November 7, 2014";          // Senior registration date nov 7
+            const string JUNIOR_DATE = "November 10, 2014";         // Junior registration date nov 10
+            const string SOPHOMORE_DATE_1 = "November 11, 2014";    // Sophomore registration date for names A-D and S-Z nov 11
+            const string SOPHOMORE_DATE_2 = "November 12, 2014";    // Sophomore registration date for names E-R nov 12
+            const string FRESHMAN_DATE_1 = "November 13, 2014";     // Freshman registration date for names A-D and S-Z nov 13
+            const string FRESHMAN_DATE_2 = "November 14, 2014";     // Freshman registration date for names E-R nov 14
+
+            const string FIRST_TIME = " 8:30 A.M.";                 // First registration time 8:30 AM
+            const string SECOND_TIME = " 10:00 A.M.";               // Second registration time 10 AM
+            const string THIRD_TIME = " 11:30 A.M.";                // Third registration time 11:30 AM
+            const string FOURTH_TIME = " 2:00 P.M.";                // Fourth registration time 2 PM
+            const string FIFTH_TIME = " 4:00 P.M.";                 // Fifth registration time 4 PM
+                      
+            char LAST_NAME = char.Parse(last_name_textbox.Text);
+            LAST_NAME = char.ToUpper(LAST_NAME); 
+        
+            if (Senior.Checked)
+                if (LAST_NAME >= 'S' && LAST_NAME <= 'Z')
+                    MessageBox.Show(SENIOR_DATE + FIRST_TIME);
+                else
+                {
+                    if (LAST_NAME >= 'A' && LAST_NAME <= 'D')
+                        MessageBox.Show(SENIOR_DATE + SECOND_TIME);
+                    else
+                    {
+                        if (LAST_NAME >= 'E' && LAST_NAME <= 'I')
+                            MessageBox.Show(SENIOR_DATE + THIRD_TIME);
+                        else
+                        {
+                            if (LAST_NAME >= 'J' && LAST_NAME <= 'O')
+                                MessageBox.Show(SENIOR_DATE + FOURTH_TIME);
+                            else
+                                MessageBox.Show(SENIOR_DATE + FIFTH_TIME);
+                        }
+                    }                   
+                }
+                if(Junior.Checked)
+                    if (LAST_NAME >= 'S' && LAST_NAME <= 'Z')
+                        MessageBox.Show(JUNIOR_DATE + FIRST_TIME);
+                    else
+                    {
+                        if (LAST_NAME >= 'A' && LAST_NAME <= 'D')
+                            MessageBox.Show(JUNIOR_DATE + SECOND_TIME);
+                        else
+                        {
+                            if (LAST_NAME >= 'E' && LAST_NAME <= 'I')
+                                MessageBox.Show(JUNIOR_DATE + THIRD_TIME);
+                            else
+                            {
+                                if (LAST_NAME >= 'J' && LAST_NAME <= 'O')
+                                    MessageBox.Show(JUNIOR_DATE + FOURTH_TIME);
+                                else                                
+                                    MessageBox.Show(JUNIOR_DATE + FIFTH_TIME);                               
+                            }
+                        }
+                    }
+                if (Sophomore.Checked)
+                    if (LAST_NAME >= 'S' && LAST_NAME <= 'T')
+                        MessageBox.Show(SOPHOMORE_DATE_1 + FIRST_TIME);
+                    else
+                    {
+                        if (LAST_NAME >= 'U' && LAST_NAME <= 'V')
+                            MessageBox.Show(SOPHOMORE_DATE_1 + SECOND_TIME);
+                        else
+                        {
+                            if (LAST_NAME >= 'W' && LAST_NAME <= 'Z')
+                                MessageBox.Show(SOPHOMORE_DATE_1 + THIRD_TIME);
+                            else
+                            {
+                                if (LAST_NAME >= 'A' && LAST_NAME <= 'B')
+                                    MessageBox.Show(SOPHOMORE_DATE_1 + FOURTH_TIME);
+                                else
+                                {
+                                    if (LAST_NAME >= 'C' && LAST_NAME <= 'D')
+                                        MessageBox.Show(SOPHOMORE_DATE_1 + FIFTH_TIME);
+                                    else
+                                    {
+                                        if (LAST_NAME >= 'E' && LAST_NAME <= 'F')
+                                            MessageBox.Show(SOPHOMORE_DATE_2 + FIRST_TIME);
+                                        else
+                                        {
+                                            if (LAST_NAME >= 'G' && LAST_NAME <= 'I')
+                                                MessageBox.Show(SOPHOMORE_DATE_2 + SECOND_TIME);
+                                            else
+                                            {
+                                                if (LAST_NAME >= 'J' && LAST_NAME <= 'L')
+                                                    MessageBox.Show(SOPHOMORE_DATE_2 + THIRD_TIME);
+                                                else
+                                                {
+                                                    if (LAST_NAME >= 'M' && LAST_NAME <= 'O')
+                                                        MessageBox.Show(SOPHOMORE_DATE_2 + FOURTH_TIME);
+                                                    else                                                    
+                                                        MessageBox.Show(SOPHOMORE_DATE_2 + FIFTH_TIME);                                                    
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+            if( Freshman.Checked)
+                if (LAST_NAME >= 'S' && LAST_NAME <= 'T')
+                    MessageBox.Show(FRESHMAN_DATE_1 + FIRST_TIME);
+                else
+                {
+                    if (LAST_NAME >= 'U' && LAST_NAME <= 'V')
+                        MessageBox.Show(FRESHMAN_DATE_1 + SECOND_TIME);
+                    else
+                    {
+                        if (LAST_NAME >= 'W' && LAST_NAME <= 'Z')
+                            MessageBox.Show(FRESHMAN_DATE_1 + THIRD_TIME);
+                        else
+                        {
+                            if (LAST_NAME >= 'A' && LAST_NAME <= 'B')
+                                MessageBox.Show(FRESHMAN_DATE_1 + FOURTH_TIME);
+                            else
+                            {
+                                if (LAST_NAME >= 'C' && LAST_NAME <= 'D')
+                                    MessageBox.Show(FRESHMAN_DATE_1 + FIFTH_TIME);
+                                else
+                                {
+                                    if (LAST_NAME >= 'E' && LAST_NAME <= 'F')
+                                        MessageBox.Show(FRESHMAN_DATE_2 + FIRST_TIME);
+                                    else
+                                    {
+                                        if (LAST_NAME >= 'G' && LAST_NAME <= 'I')
+                                            MessageBox.Show(FRESHMAN_DATE_2 + SECOND_TIME);
+                                        else
+                                        {
+                                            if (LAST_NAME >= 'J' && LAST_NAME <= 'L')
+                                                MessageBox.Show(FRESHMAN_DATE_2 + THIRD_TIME);
+                                            else
+                                            {
+                                                if (LAST_NAME >= 'M' && LAST_NAME <= 'O')
+                                                    MessageBox.Show(FRESHMAN_DATE_2 + FOURTH_TIME);
+                                                else                                                
+                                                    MessageBox.Show(FRESHMAN_DATE_2 + FIFTH_TIME);                                               
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                                             
+
+
+
+                
+            
+            
+            
+            
+        }
+    }
+}
